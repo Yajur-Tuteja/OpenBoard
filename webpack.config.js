ddce9f7bf0d324421d54bfd0465441b53d226819
Yajur Tuteja <yajur.t@commerceiq.ai>
2024-01-06 22:35:59 +0530

    resolving socket error path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: {
-        bundle: path.resolve(__dirname, 'app.js'),
+        bundle: path.resolve(__dirname, 'public/app.js'),
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
