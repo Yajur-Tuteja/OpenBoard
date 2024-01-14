c23ecd636d2c8b437840d106e6f55186f7149dcc
Yajur Tuteja <yajur.t@commerceiq.ai>
2024-01-14 18:44:30 +0530

    no socket

--- public/app.js
+++ public/app.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const socket = require("socket.io");
+// const express = require("express");
+// const socket = require("socket.io");
 
-const app = express(); // initialize and server ready
+// const app = express(); // initialize and server ready
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-let port = process.env.PORT || 5000;
-let server = app.listen(port, () => {
-    console.log("Listening to port " + port);
-});
+// let port = process.env.PORT || 5000;
+// let server = app.listen(port, () => {
+//     console.log("Listening to port " + port);
+// });
 
-let io = socket(server);
+// let io = socket(server);
 
-io.sockets.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-});
+// io.sockets.on("connect_error", (err) => {
+//     console.log(`connect_error due to ${err.message}`);
+// });
 
-io.sockets.on("connection", (socket) => {
-    console.log("Made socket connection");
+// io.sockets.on("connection", (socket) => {
+//     console.log("Made socket connection");
 
-    // Received data
-    socket.on("beginPath", (data) => {
-        // transfer data to all connected devices
-        io.sockets.emit("beginPath", data);
-    });
+//     // Received data
+//     socket.on("beginPath", (data) => {
+//         // transfer data to all connected devices
+//         io.sockets.emit("beginPath", data);
+//     });
 
-    socket.on("drawStroke", (data) => {
-        // transfer data to all connected devices
-        io.sockets.emit("drawStroke", data);
-    });
+//     socket.on("drawStroke", (data) => {
+//         // transfer data to all connected devices
+//         io.sockets.emit("drawStroke", data);
+//     });
 
-    socket.on("undoRedo", (data) => {
-        // transfer data to all connected devices
-        io.sockets.emit("undoRedo", data);
-    });
-});
+//     socket.on("undoRedo", (data) => {
+//         // transfer data to all connected devices
+//         io.sockets.emit("undoRedo", data);
+//     });
+// });
 
 
